dd7620b65b812f9c39eafd56a295c915177bfc23
Perttu Punakallio <perttu@Perttus-Air.home>
2019-06-25 21:32:17 +0300

    Finish exercise 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const Numbers = ({persons}) => {
+    const rows = () => persons.map((person) => <div key={person.name}>{person.name}</div>)
+    return (
+      <>
+        {rows()}
+      </>
+    )
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Numbers persons={persons} />
+    </div>
+  )
+
+}
+
+export default App

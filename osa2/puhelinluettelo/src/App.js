bca8a7dff7fc885d3d5ebda6748729dd8d2c922f
Perttu Punakallio <perttu@Perttus-Air.home>
2019-06-25 23:41:29 +0300

    Finish exercise 2.9*: puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
-  ]) 
+  const [ persons, setPersons] =  useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   const Persons = ({persons}) => {
     const rows = () => persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)
@@ -31,6 +37,7 @@ const App = () => {
 
     
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
@@ -39,11 +46,13 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input></input></div>
+      <div>filter shown with <input value={filter} onChange={handleFilterChange}></input></div>
       <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
@@ -57,7 +66,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={personsToShow} />
     </div>
   )
 

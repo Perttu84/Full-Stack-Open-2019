fcee17561b8cc5d3169f548c17080774b2d62b0c
Perttu Punakallio <perttu@Perttus-Air.home>
2019-07-31 23:50:29 +0300

    Add error handling if new blog is missing title or url

--- osa4/app.js
+++ osa4/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
@@ -12,5 +13,6 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,16 +1,21 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
 
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog.toJSON())
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter

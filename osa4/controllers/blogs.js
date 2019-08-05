2af0e4462a2f7d6b1bcb311b2c4e214c5db59bd5
Perttu Punakallio <perttu@Perttus-Air.home>
2019-08-05 21:42:26 +0300

    Add possibility to delete single blog and add error for malformatted id in API calls

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,6 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -13,6 +12,15 @@ blogsRouter.post('/', async (request, response, next) => {
   try {
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
   } catch(exception) {
     next(exception)
   }

17ecd6998ab64407f1c006f2b8a9d12561ecaefc
Perttu Punakallio <perttu@Perttus-Air.home>
2019-08-06 22:15:52 +0300

    Finish exercise 4.15: blogilistan laajennus, step4

--- osa4/app.js
+++ osa4/app.js
@@ -4,15 +4,17 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(config.MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app

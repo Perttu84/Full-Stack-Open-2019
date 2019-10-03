54691ff8c640ef5097bb8f710edd3014b38dc762
Perttu Punakallio <perttu@Perttus-Air.home>
2019-10-03 22:48:43 +0300

    Finish exercise 7.11

--- osa7/bloglist/frontend/src/components/BlogDetailedView.js
+++ osa7/bloglist/frontend/src/components/BlogDetailedView.js
@@ -20,25 +20,44 @@ const BlogDetailedView = (props) => {
     setLikes(likes+1)
   }
 
+  const Comments = ({comments}) => {
+    if (comments.length > 0) {
+      return (
+        <div>
+          <h3>Comments</h3>
+            <ul>
+              {comments.map(comment =>
+                <li key={comment.id}>{comment.content}</li>
+              )}
+            </ul>
+        </div>
+      )
+    }
+    return null
+  }
+
   if (user.username !== blog.user.username) {
     return (
       <div className='blog'>
-        {blog.title} {blog.author}<br/>
+        <h2>{blog.title} {blog.author}</h2>
         <a href={blog.url}>{blog.url}</a><br/>
         {likes} likes
         <button onClick={handleLikeClick}>like</button><br/>
         added by { blog.user.name }
+        <Comments comments={blog.comments} />
       </div>
+      
     )
   }
   return (
     <div className='blog'>
-      {blog.title} {blog.author}<br/>
+      <h2>{blog.title} {blog.author}</h2>
       <a href={blog.url}>{blog.url}</a><br/>
       {likes} likes
       <button onClick={handleLikeClick}>like</button><br/>
       added by { blog.user.name }<br/>
       <button onClick={() => handleRemoveClick(blog)}>remove</button>
+      <Comments comments={blog.comments} />
     </div>
   )
 }
